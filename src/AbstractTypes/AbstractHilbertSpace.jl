acfcbec9e37e039a14f00943afaa63f2bb56dacf
Nils Niggemann <nils.niggemann@fu-berlin.de>
2025-03-16 15:59:48 +0100

    constraint overhaul

--- src/AbstractTypes/AbstractHilbertSpace.jl
+++ src/AbstractTypes/AbstractHilbertSpace.jl
@@ -1,11 +1,11 @@
 """
     AbstractHilbertSpace
 
-An abstract type representing a Hilbert space. This type serves as a base for defining various specific Hilbert spaces used in the project. Generally, a hilbert space should be defined by the number of sites, the number of local degrees of freedom (i.e. Spin), and the constraints that the configurations must satisfy. 
+An abstract type representing a Hilbert space. This type serves as a base for defining various specific Hilbert spaces used in the project. Generally, a hilbert space should be defined by the number of sites, the number of local degrees of freedom (i.e. Spin), and the constraint that the configurations must satisfy. 
 # Interface
-- `constraints(HilbertSpace::AbstractHilbertSpace)`: Return the constraints that the configurations in the Hilbert space must satisfy.
-- `Base.size(HilbertSpace::AbstractHilbertSpace)`: Return the size of the Hilbert space.
-- fulfills_constraint(x,HilbertSpace::AbstractHilbertSpace): Check if the given configuration `x` satisfies the constraints of the specified `HilbertSpace`.
+- `constraint(HilbertSpace::AbstractHilbertSpace)`: Return the constraint that the configurations in the Hilbert space must satisfy.
+- `Base.size(HilbertSpace::AbstractHilbertSpace)`: Return the size of a config in the Hilbert space.
+- fulfills_constraint(x,HilbertSpace::AbstractHilbertSpace): Check if the given configuration `x` satisfies the constraint of the specified `HilbertSpace`.
 """
 abstract type AbstractHilbertSpace end
 
@@ -18,19 +18,27 @@ An abstract type representing a constraint in the context of a Hilbert space.
 """
 abstract type AbstractConstraint end
 
-"""get the constraints that the configurations in the Hilbert space must satisfy."""
-function constraints end
+struct MultipleConstraints{Cons<:Tuple} <: AbstractConstraint
+    constraints::Cons
+end
+(c::MultipleConstraints)(x) = all(c_i(x) for c_i in c.constraints)
+
+struct NoConstraint <: AbstractConstraint end
+(cons::NoConstraint)(x) = true
+
+"""get the constraint that the configurations in the Hilbert space must satisfy."""
+function constraint end
 
 """
     fulfills_constraint(x::AbstractArray, HilbertSpace::AbstractHilbertSpace)
 
-Check if the given configuration `x` satisfies the constraints of the specified `HilbertSpace`.
+Check if the given configuration `x` satisfies the constraint of the specified `HilbertSpace`.
 
 # Arguments
 - `x::AbstractArray`: The configuration to be checked.
-- `HilbertSpace::AbstractHilbertSpace`: The Hilbert space whose constraints need to be satisfied.
+- `HilbertSpace::AbstractHilbertSpace`: The Hilbert space whose constraint need to be satisfied.
 
 # Returns
-- `Bool`: `true` if the configuration satisfies the constraints, `false` otherwise.
+- `Bool`: `true` if the configuration satisfies the constraint, `false` otherwise.
 """
 function fulfills_constraint end
