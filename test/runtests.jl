354ee5a6faf3c344a2f6f0578bbaede521fd8a30
Nils Niggemann <nils.niggemann@fu-berlin.de>
2025-06-06 17:42:06 +0200

    cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -283,7 +283,6 @@ end
     runGFMC!(prob, NoObserver(), 200; rng = RNG)
     runGFMC!(prob, Observer, NSteps; rng = RNG)
 
-    println(BObs.TotalWeights[end])
     Energy = GFMC.getEnergies(BObs.TotalWeights, BObs.energies, mProj)
     Energy_direct = BasicAccumulatorFile.en_numerator ./ BasicAccumulatorFile.Gnp_denominator .*NSites
 
@@ -333,52 +332,3 @@ end
         end
     end
 end
-
-##
-include("utils.jl")
-
-function E_critPoint_exact(L)
-    return 1 - csc(pi / (2 * (2 * L + 1)))
-end
-σz(n::Bool) = (1 - 2 * n)
-σz(i, conf::AbstractArray) = σz(conf[i])
-
-using GreenFunctionMonteCarlo.LinearAlgebra
-NSites = 2
-NSteps = 1500
-mProj = 20
-NWalkers = 2
-
-RNG = StableRNG(1234)
-
-Hilbert = BosonHilbertSpace(NSites, HardCoreConstraint())
-moves = eachcol(Bool.(I(NSites))) # each move flips a single spin
-offdiagElements = -ones(NSites)
-Hxx = DiagOperator(x-> sum(σz(i, x) * σz(i + 1, x) for i in eachindex(x)[1:end-1]))
-
-H = localOperator(moves, offdiagElements, Hxx, Hilbert)
-
-config = BosonConfig(Hilbert)
-rand!(RNG,config)
-logψ = GFMC.EqualWeightSuperposition()
-CT = ContinuousTimePropagator(0.1)
-
-prob = GFMCProblem(config, NWalkers, CT; logψ, H, Hilbert)
-
-outfile = tempname()
-
-BObs = GFMC.BasicObserver(outfile, NSteps, NWalkers)
-CObs = GFMC.ConfigurationObserver(outfile, config, NSteps, NWalkers)
-
-BasicAccumulatorFile = GFMC.BasicAccumulator(outfile, mProj, NWalkers)
-ObsAccumulatorFile = GFMC.ObservableAccumulator(outfile,OccupationNumber(NSites),BasicAccumulatorFile, mProj, NWalkers, Threads.nthreads())
-
-# Observer = GFMC.CombinedObserver((BObs, CObs,BasicAccumulatorFile, ObsAccumulatorFile))
-Observer = GFMC.CombinedObserver((BObs, CObs))
-
-RNG = StableRNG(1234)
-runGFMC!(prob, NoObserver(), 200; rng = RNG)
-runGFMC!(prob, Observer, NSteps; rng = RNG)
-
-println(BObs.TotalWeights[end])
-println(prob.Walkers.Configs[1])
